692cbcf49808ecec3d98d18c0e129b7f67685e3b
Grey <greywhat2002@gmail.com>
2024-08-03 12:37:02 +0200

    fixed pycodestyle issue

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """module contains a type-annotated function sum_list"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """returns the sum of a list of floats"""
     return sum(input_list)
