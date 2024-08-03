4f0849303b659036b68b338b08b52a4693f8d011
Grey <greywhat2002@gmail.com>
2024-08-03 13:30:24 +0200

    fixed duck typing

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module contains a type-annotated function"""
+from typing import Any, Mapping, TypeVar, Union
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default:
+                      Union[T, None] = None) -> Union[Any, T]:
+    """returns the value of a key in a dictionary"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
