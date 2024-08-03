3f0819cd292b4c1199b400041fbe920eabc73a7e
Grey <greywhat2002@gmail.com>
2024-08-03 12:35:43 +0200

    made function compatible with python 7 type annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """module contains a type-annotated function sum_list"""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns the sum of a list of floats"""
     return sum(input_list)
