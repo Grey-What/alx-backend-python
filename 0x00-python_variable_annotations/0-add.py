a617370772204f58d824db0a04c756e521460040
Grey <greywhat2002@gmail.com>
2024-08-03 12:08:08 +0200

    type annoted function adding two floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""moudle contains a type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    adds two floats
+
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        sum of a and b
+    """
+    return a + b
