92cc6e3a4e2a72f94c2c1e9343c389c1fa30cc54
Grey <greywhat2002@gmail.com>
2024-08-06 15:08:53 +0200

    fixed type annotation for return

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """module contains a coroutine called async_generator"""
 import random
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """coroutine that takes no arguments that yields a random value"""
     for _ in range(10):
         await asyncio.sleep(1)
