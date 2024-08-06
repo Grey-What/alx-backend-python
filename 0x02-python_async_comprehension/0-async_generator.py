76a66f1967f5176493af9041fa16854499fa41c8
Grey <greywhat2002@gmail.com>
2024-08-06 15:00:35 +0200

    async generator of 10 random values

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module contains a coroutine called async_generator"""
+import random
+from typing import AsyncGenerator
+import asyncio
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """coroutine that takes no arguments that yields a random value"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
