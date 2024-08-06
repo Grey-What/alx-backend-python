7ad261591eba74c6371df3edd54f3b0411f6813c
Grey <greywhat2002@gmail.com>
2024-08-06 11:53:18 +0200

    swapped asyncio method with for loop

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,5 +9,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """returns a lsit of delays"""
-    return [await wait_random(max_delay) for x in range(n)]
+    """
+    executes wait_random coroutine n times and
+    returns a lsit of delays
+    """
+    delays = []
+    for x in range(n):
+        delays.append(await wait_random(max_delay))
+    return delays
