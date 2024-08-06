3c400e9208536a0e991e3e4105d3d41181a3d4bc
Grey <greywhat2002@gmail.com>
2024-08-06 12:20:53 +0200

    fixed pycodestyle issue

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -13,5 +13,6 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     executes task_wait_random coroutine n times and
     returns a lsit of delays
     """
-    delays = await asyncio.gather(*[task_wait_random(max_delay) for _ in range(n)])
+    delays = await asyncio.gather(
+        *[task_wait_random(max_delay) for _ in range(n)])
     return sorted(delays)
