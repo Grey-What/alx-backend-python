9da21768f9e819c39ffcb3bc07f62f67b1a607b3
Grey <greywhat2002@gmail.com>
2024-08-29 12:28:07 +0200

    integration testing with fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,8 @@ import unittest
 from parameterized import parameterized
 from unittest.mock import patch, MagicMock, Mock, PropertyMock
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from requests import HTTPError
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -101,3 +103,47 @@ class TestGithubOrgClient(unittest.TestCase):
         client_has_license = org_client.has_license(repo, license_key)
 
         self.assertEqual(client_has_license, expected)
+
+
+@parameterized.expand([
+    {
+        'org_payload': TEST_PAYLOAD[0][0],
+        'repos_payload': TEST_PAYLOAD[0][1],
+        'expected_repos': TEST_PAYLOAD[0][2],
+        'apache2_repos': TEST_PAYLOAD[0][3]
+    }
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """test integration class for GithubOrgClient"""
+    @classmethod
+    def setUpClass(cls) -> None:
+        route_payloads = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url: str):
+            if url in route_payloads:
+                return Mock(**{'josn.return_value': route_payloads[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """test public_repos"""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """test public_repos with license"""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.get_patcher.stop()
